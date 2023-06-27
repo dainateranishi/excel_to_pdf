a4289039906b1ebab834903b2d6e0e3da9630508
Daina Teranishi <daina.teranishi@gmail.com>
2023-06-27 22:55:32 +0900

    DataFrameをHTMLに変換してからPDF出力

--- workspace/functions/excel_to_pdf.py
+++ workspace/functions/excel_to_pdf.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from xhtml2pdf import pisa
 
 def load_excel(path: str) -> pd.DataFrame:
     """excelファイルを読み込む
@@ -12,3 +13,17 @@ def load_excel(path: str) -> pd.DataFrame:
     """
 
     return pd.read_excel(path, sheet_name=None, index_col=0)
+
+def output_to_pdf(
+        data: pd.DataFrame,
+        output_path: str
+        ) -> None:
+    """DataFrameをPDFファイルへ出力する
+
+    Args:
+        data (pd.DataFrame): _description_
+    """
+
+    with open(output_path, "w+b") as file:
+        pisa.CreatePDF(src=data.to_html(), dest=file)
+
